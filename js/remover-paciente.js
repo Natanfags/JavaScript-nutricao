e458755d1cf50725902be4b3e942d4ea443f7ecc
Natan Fagundes <natan.fags@gmail.com>
2019-01-07 12:30:17 -0200

    Implementado para apagar apneas o td

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,10 +1,9 @@
-var tabela = document.querySelector("table");
+var tabela = document.querySelector('table');
 
-tabela.addEventListener("dblclick",function(event){
+tabela.addEventListener('dblclick', function(event) {
 
-    event.target.parentNode.classList.add("fadeOut");
-
-    setTimeout(function(){
-        event.target.parentNode.remove();
-    }, 500);
+    // Somente executará nosso código caso o elemento em que clicamos seja um <td>
+    if (event.target.tagName == 'TD') {
+        event.target.parentNode.remove()
+    }
 });
